acee476eaeb8910ec95168bb128bb624a5b8d1a7
opazisimo <suopazo@4talent.cl>
2017-01-16 12:26:18 -0300

    Initial commit2

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+
+
+gulp.task('webserver', function() {
+  gulp.src('dist/')
+    .pipe(webserver({
+      fallback: 'index.html',
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('script', function(){
+  gulp.src('src/js/custom.js')
+  .pipe(concat('script.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('image', function() {
+     gulp.src('src/**/*.{jpg,jpeg,gif,png,svg}')
+   .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('style', function(){
+  gulp.src('src/sass/global.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCSS())
+  .pipe(concat('style.min.css'))
+  .pipe(gulp.dest('dist/css'))
+
+});
+
+gulp.task('default', ['style','script','image','webserver']);
