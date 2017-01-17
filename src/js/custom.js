b04c2fa7343ee4cd355345f868f070a30129e7b3
andrea <asuaruiz@gmail.com>
2017-01-17 09:42:44 -0300

    custom

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,38 +1,25 @@
 $('.form-rut').rut();
 //$('#btn-login').preventDefault()
 
-//LOG IN BCI
-/*var request = require("request");
-var rut = $('#rut').val();
-var dv = $('#dv').val();
-var password = $('#password').val()
-var options = { method: 'POST',
-  url: 'https://api.us.apiconnect.ibmcloud.com/hackaton-2016-produccion-master/api/cliente/login',
-  headers:
-   { rut: body.rut,
-     accept: 'application/json',
-     'content-type': 'application/json' },
-  body: { rut: rut, dv: dv, password: password },
-  json: true };
 
-request(options, function (error, response, body) {
-  if (error) return console.error('Failed: %s', error.message);
 
-  console.log('Success: ', body);
-});
 
 //RECUPERAR DATOS PRODUCTOS DE CLIENTE
-var request = require("request");
+var rut = $('#rut').val();
 
-var options = { method: 'GET',
-  url: 'https://api.us.apiconnect.ibmcloud.com/hackaton-2016-produccion-master/api/cliente/cuentas',
-  headers:
-   { rut: rut,
-     accept: 'application/json',
-     'content-type': 'application/json' } };
+var ajaxProductos = function(productos){
+    $.ajax({
+     url: 'https://api.us.apiconnect.ibmcloud.com/hackaton-2016-produccion-master/api/cliente/cuentas',
+     type: 'GET',
+     datatype: 'json',
+     headers: {
+        "rut" : "11114444-9"
+     }
+   })
+   .done(function(response) {
+     console.log(response);
+   })
+   .fail(function() {
+     console.log("error");
+   });  }
 
-request(options, function (error, response, body) {
-  if (error) return console.error('Failed: %s', error.message);
-
-  console.log('Success: ', body);
-});*/
